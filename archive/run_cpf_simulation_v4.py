a1420e054b6e12ca036a87e8308a9e1a836b896a
Jose Ibay <ourcatisfat@gmail.com>
2025-05-09 02:37:58 +0800

    cleanup

--- archive/run_cpf_simulation_v4.py
+++
@@ -1,7 +0,0 @@
-from src.cpf_program_v7 import CPFAccount
-
-if __name__ == "__main__":
-    # Entry point for CPF simulation using correct logic
-    print("Running CPF simulation using cpf_program_v7...")
-    cpf_account = CPFAccount(config_file='new_config.json')
-    cpf_account.run_simulation()
